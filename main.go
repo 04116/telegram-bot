efc602c3a270974894494f01860cf4471d880d41
phuclm <phuclm@mirabo.com.vn>
2022-08-26 13:30:11 +0700

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+}
